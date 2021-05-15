8365d7acb66bb3c3bb74cdb72dec7711d4a84198
zhanggang20 <17310856195@163.com>
2021-05-15 16:38:48 +0800

    初始化

---
+++ .vuepress/config.js
@@ -0,0 +1,96 @@
+module.exports = {
+  "title": "海边看日落",
+  "description": "",
+  "dest": "public",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.jpg"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "主页",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "时间线",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "最新发布",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "第一篇文章？",
+            "link": "/docs/theme-reco/"
+          }
+        ]
+      },
+      {
+        "text": "联系我",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "619956910",
+            //"link": "https://github.com/recoluan",
+            "link": "",
+            "icon": "reco-qq"
+          }
+        ]
+      }
+    ],
+    "sidebar": {
+      "/docs/theme-reco/": [
+        "",
+        "theme",
+        "plugin",
+        "api"
+      ]
+    },
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "分类"
+      },
+      "tag": {
+        "location": 3,
+        "text": "标签"
+      }
+    },
+    "friendLink": [
+      {
+        "title": "南上康青山",
+        "desc": "一个吊炸天的前端大牛，接诉即办前端负责人，前端架构师，敢和韩总监正面硬刚的汉子！",
+        "email": "1156743527@qq.com",
+        "link": "https://www.qiyoe.cn"
+      },
+    ],
+    "logo": "/logo.jpg",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "lastUpdated": "Last Updated",
+    "author": "ZhangGang",
+    "authorAvatar": "/zg.jpg",
+    "record": "京ICP备2021014403号-2",
+    "startYear": "2021"
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+}
---
+++ docs/theme-reco/README.md
@@ -0,0 +1,7 @@
+---
+title: 第一篇文章？
+date: 2020-05-29
+---
+
+This is theme-reco.
+這是啥
